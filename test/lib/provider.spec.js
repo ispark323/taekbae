edd2b37a8e60c2706bd292e221a79c87080cfc4b
Moo Yeol, Lee (Prescott) <mooyoul@gmail.com>
2016-04-08 03:17:46 +0900

    test: Added `Provider` spec

---
+++ test/lib/provider.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Provider = require('../../lib/provider');
+
+describe('Provider Class', function () {
+  
+  it('should export Provider constructor', function () {
+    expect(Provider).to.be.a('function');
+  });
+
+});
